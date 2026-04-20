bafe26d1f434887960245024bfb6979630890aed
Abdullah Al Noman <140273645+nomanstine@users.noreply.github.com>
2026-04-20 21:58:22 +0000

    Refactor environment variable handling in Docker Compose and application configuration

--- backend/src/main/java/just/inventory/backend/config/JwtUtil.java
+++ backend/src/main/java/just/inventory/backend/config/JwtUtil.java
@@ -17,7 +17,7 @@ import java.util.function.Function;
 @Component
 public class JwtUtil {
 
-    @Value("${jwt.secret:404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970}")
+    @Value("${jwt.secret}")
     private String secret;
 
     @Value("${jwt.expiration:86400000}") // 24 hours in milliseconds
